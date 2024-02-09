65118d66adcf8ab643a25cfbfdaeb22a1d80c078
Sergeeva Larisa <mindzu@mail.ru>
2024-02-09 17:13:04 +0500

    try decorators competition

---
+++ competitions/id2.py
@@ -0,0 +1,79 @@
+def simple_input(_, prompt):
+    """Just input string."""
+    return input(prompt)
+
+
+def asks(name, prompt):
+    """
+    Описывает один запрос аргумента.
+
+    Arguments:
+        name - имя аргумента оборачиваемой функции,
+
+        prompt - текст приглашения.
+
+    Returns:
+        декоратор, который и обернёт нужную функцию. Помните, после
+        описания всех аргументов нужно результат обернуть в
+        декоратор interactive!
+
+    """
+    # BEGIN (write your solution here)
+    def wrapper(func):
+
+        def inner(*args, **kwargs):
+            # arg = simple_input(name, prompt)
+            # print('name =', arg)
+            return func(name, prompt)
+            # return func(*args, **kwargs)
+        return inner
+    return wrapper
+    # END
+
+
+def interactive(
+    description,
+    input_function=simple_input,
+    output_function=print,
+):
+    """
+    Делает функцию с описанными параметрами интерактивной.
+
+    Интерактивной может быть функция без параметров. В этом случае декоратор
+    asks не потребуется.
+
+    Arguments:
+        description - описание, которое будет выведено в начале
+        диалога (интерактивной сессии).
+
+        input_function - функция, принимающая имя аргумента и приглашение,
+        и возвращающая введённый пользователем текст (str).
+
+        output_function - функция, которая будет использована для вывода
+        описания и результата вызова оборачиваемой функции.
+
+    Returns:
+        декоратор, который обернёт целевую функцию.
+
+    """
+    # BEGIN (write your solution here)
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            output_function(description)
+            arg = asks()(input_function)(*args, **kwargs)
+            result = func(arg, *args, **kwargs)
+            output_function(result)
+        return inner
+    return wrapper
+    # END
+
+
+@interactive('Calculator')
+@asks('x', 'Enter first number: ')
+@asks('y', 'Enter second number: ')
+def calc(x, y):
+    return int(x) + int(y)
+
+
+if __name__ == '__main__':
+    calc()
---
+++ competitions/test_id2.py
@@ -0,0 +1,81 @@
+from id2 import asks, interactive
+
+
+def non_interactive(**kwargs):
+    """
+    Заменяет собой interactive, симулируя диалог с пользователь.
+
+    Вместо запросов на ввод будут подставлены указанные значения,
+    а функция в итоге вернёт результат вызова оригинальной функции.
+
+    Arguments:
+        kwargs - параметры для подстановки. Значения должны быть строками.
+
+    Returns:
+        декоратор, который обернёт нужную функцию.
+
+    """
+    def wrapper(function):
+        def inner():
+            output = []
+            interactive(
+                '',
+                input_function=lambda key, _: kwargs[key],
+                output_function=output.append,
+            )(function)()
+            return output[1]  # нужен только "выведенный" результат
+        return inner
+    return wrapper
+
+
+def test_interactive():
+    def should_not_input(*_):
+        raise AssertionError("Shouldn't ask for input")
+
+    output = []
+
+    @interactive(
+        'Foo',
+        input_function=should_not_input,
+        output_function=output.append,
+    )
+    def bar():
+        return 'Bar'
+
+    assert bar() is None, "Interactive function should not return something!"
+    assert output == ['Foo', 'Bar']
+
+
+def test_non_interactive():
+    @non_interactive()
+    def foo():
+        return 42
+
+    assert foo() == 42
+
+
+def test_asks():
+    @non_interactive(x='42', y='57')
+    @asks('x', 'Enter first number: ')
+    @asks('y', 'Enter second number: ')
+    def add(x, y):
+        return int(x) + int(y)
+
+    assert add() == 99
+
+
+def test_asks_order():
+    order = []
+
+    @interactive(
+        '',
+        input_function=lambda key, _: order.append(key) or '',
+        output_function=lambda _: None,
+    )
+    @asks('a', '?')
+    @asks('b', '??')
+    def two_args(a, b):
+        return a + b
+
+    two_args()
+    assert order == ['a', 'b'], "Questions should be asked in ceratin order!"
